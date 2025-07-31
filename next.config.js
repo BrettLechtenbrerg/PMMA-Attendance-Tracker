a0509e207625615b74ad1ec02b0b2e9411807ce7
BrettLechtenbrerg <76498627+BrettLechtenbrerg@users.noreply.github.com>
2025-07-30 21:26:51 -0600

    Exclude Supabase functions from TypeScript build

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['images.leadconnectorhq.com', 'assets.cdn.filesafe.space'],
-  },
-}
-
-module.exports = nextConfig
